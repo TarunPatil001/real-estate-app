dfc07f487f35cb1c1fb419515535363f83cca35c
Tarun Kishor Patil <tarunpatil001@gmail.com>
2024-11-23 15:20:22 +0530

    Testing the hosted app.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1 className='text-red-500'>App</h1>
+      <h1 className='text-red-500'>Online Hosted</h1>
     </div>
   )
 }
