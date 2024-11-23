fcba82e3821d3b63740b6f0ca76611d3e8f3f179
Tarun Kishor Patil <tarunpatil001@gmail.com>
2024-11-23 20:48:07 +0530

    error resolve - in import SignIn, About

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/signIn';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import About from './pages/about';
+import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 
